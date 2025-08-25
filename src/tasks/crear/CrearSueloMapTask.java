b9c5bff7e312d7209e5749fd9664fcb344fa0b02
Tomas Lund Petersen <kotogadekiru@gmail.com>
2025-08-25 18:04:17 -0300

    fix suelo tooltip

--- src/tasks/crear/CrearSueloMapTask.java
+++ src/tasks/crear/CrearSueloMapTask.java
@@ -122,61 +122,95 @@ public class CrearSueloMapTask extends ProcessMapTask<SueloItem,Suelo> {
 		updateProgress(0, featureCount);
 
 	}
+	
+	public static String getLabelForNutriente(Nutriente n) {
+		NumberFormat df = Messages.getNumberFormat();
+		df.setMaximumFractionDigits(0);
+		Double profundidad = n.getProfundidad()*100;				
+		String prof = df.format(profundidad);
+		df.setMaximumFractionDigits(2);
+		df.setMinimumFractionDigits(2);
+		
+		return n.getSimbolo()+"(0-"+prof+"cm): ";
+		
+	}
 
 	public static String buildTooltipText(SueloItem si,double area) {
 		NumberFormat df = Messages.getNumberFormat();//new DecimalFormat("#,###.##");//$NON-NLS-2$
 
 		StringBuilder sb = new StringBuilder();
+		sb.append("______________________\n");
 		//Fosforo		
-		sb.append(df.format(si.getPpmP()));
-		sb.append(" " + Messages.getString("OpenSoilMapTask.2") + " 0-20cm \n "); //ppm
+		Map<SueloParametro, Nutriente> spd = Nutriente.getNutrientesDefault();
+		Nutriente fosforo =spd.get(SueloParametro.Fosforo);		
+		sb.append(getLabelForNutriente(fosforo)+df.format(si.getPpmP())+"ppm\n");
+		//sb.append(" " + Messages.getString("OpenSoilMapTask.2") + " 0-20cm \n "); //ppm
 
-		//Nitrogeno
-		sb.append(df.format(si.getPpmNO3()));
-		sb.append(" " + Messages.getString("OpenSoilMapTask.4") + " 0-60cm \n");//Ppm NO3
+		//Nitrogeno	//FIXME verificar si es N-NO3 o NO3			
+		sb.append(getLabelForNutriente(spd.get(SueloParametro.Nitrogeno))+df.format(si.getPpmNO3())+"ppm\n");
+		//sb.append("NO3(0-60): "+df.format(si.getPpmNO3())+"ppm\n");
 
-		sb.append(df.format(Suelo.getKgNHa(si)));
-		sb.append(" kg/ha N 0-60cm \n");// por kg/ha
+		sb.append("           "+df.format(Suelo.getKgNHa(si))+"kgN/ha\n");// por kg/ha
+		
+		//POTASIO
+		Double ppmK = si.getPpmK();
+		if(ppmK != null && Math.abs(ppmK)>0)
+		sb.append(getLabelForNutriente(spd.get(SueloParametro.Potasio))+df.format(ppmK)+"ppm\n");
+		//sb.append("K(0-20): "+df.format(si.getPpmK())+"ppm\n");
 
-		sb.append(df.format(si.getPpmK()));		
-		sb.append(" Ppm K 0-20cm \n");
-
-		sb.append(df.format(si.getPpmS()));
-		sb.append(" Ppm S 0-20cm \n");
+		//AZUFRE
+		Double ppmS = si.getPpmS();
+		if(ppmS != null && Math.abs(ppmS)>0)
+		sb.append(getLabelForNutriente(spd.get(SueloParametro.Azufre))+df.format(ppmS)+"ppm\n");
+		//sb.append("S(0-20): "+df.format(si.getPpmS())+"ppm\n");
+		//sb.append(df.format(si.getPpmS()));
+		//sb.append("Ppm-S(0-20cm)\n");
 
 		
 		List<SueloParametro> microNutrientes = Nutriente.getMicroNutrientes();
 		microNutrientes.forEach((sp)->{
 			Nutriente n = Nutriente.getNutrientesDefault().get(sp);
 			Double ppm = Suelo.getPpm(sp, si);
-			if(ppm != null ) {				
-				sb.append(df.format(ppm));
-				df.setMaximumFractionDigits(0);
-				Double profundidad = n.getProfundidad()*100;				
-				String prof = df.format(profundidad);
-				df.setMaximumFractionDigits(2);
-				df.setMinimumFractionDigits(2);
-				sb.append(" Ppm "+n.getSimbolo()+" 0-"+prof+"cm \n");
+			if(ppm != null && Math.abs(ppm)>0) {			
+				sb.append(getLabelForNutriente(n));
+				sb.append(df.format(ppm)+"ppm\n");				
 			}
 		});
 		
+		//MATERIA ORGANICA
+		sb.append("MO(0-20): "+df.format(si.getPorcMO())+"%\n");
+	//	sb.append(df.format(si.getPorcMO()));
+	//	sb.append("% " + Messages.getString("JFXMain.236") + " 0-20cm \n ");//JFXMain.236=%M0
 
-
-		sb.append(df.format(si.getPorcMO()));
-		sb.append("% " + Messages.getString("JFXMain.236") + " 0-20cm \n ");//JFXMain.236=%M0
-
-		sb.append(df.format(si.getDensAp()));
-		sb.append(" " + "dens" + " kg/m3 \n "); //kg/m3
+		//DENSIDAD APARENTE
+		df.setMaximumFractionDigits(0);
+		sb.append("DA(0-60): " + df.format(si.getDensAp()) + "kg/m³\n");//³=alt+0179
+		df.setMaximumFractionDigits(2);
+		df.setMinimumFractionDigits(2);
+		//sb.append(df.format(si.getDensAp()));
+		//sb.append(" " + "dens" + " kg/m3 \n "); //kg/m3
+		
+		//OTROS
+		//getProfNapa(),
+		//getAguaPerfil(),		
+		//getTextura(),
+		//getPorosidad(),
+		//getPorcCC()
+		
+		//ELEVACION
 		sb.append(Messages.getString("ProcessHarvestMapTask.25")
 				+df.format(si.getElevacion() ) 
-				+ Messages.getString("ProcessHarvestMapTask.26")); 
+				+ "m\n"); 
+		
+		//SUPERFICIE
 		if(area<1){
-			sb.append( Messages.getString("CrearSueloMapTask.sup")+": "+df.format(area * ProyectionConstants.METROS2_POR_HA) + "m2\n");
+			sb.append( Messages.getString("CrearSueloMapTask.sup")+": "+df.format(area * ProyectionConstants.METROS2_POR_HA) + "m²\n");
 		} else {
 			sb.append( Messages.getString("CrearSueloMapTask.sup")+": "+df.format(area ) + "Has\n");
 		}
 
 		String tooltipText = sb.toString();
+		//System.out.println(tooltipText);
 		return tooltipText;
 	}
 
